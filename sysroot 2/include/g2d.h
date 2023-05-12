f4defcd31b8af317726c4d7b46e36472b3357848
Giuseppe Ricciardi <102236495+GRicciardi00@users.noreply.github.com>
2023-05-12 20:36:45 +0200

    main working

--- sysroot 2/include/g2d.h
+++
@@ -1,9 +0,0 @@
-#ifndef G2D_H_
-#define G2D_H_
-
-#define WASM_EXPORT __attribute__((__visibility__("default")))
-
-extern "C" WASM_EXPORT void g2d_debug() {}
-
-#undef WASM_EXPORT
-#endif // G2D_H_
